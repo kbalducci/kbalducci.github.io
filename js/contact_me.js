5d51e2b085f97bc078f4cf6e0f4154f19002877e
Kate <kbalducci@hotmail.com>
2015-04-23 00:58:55 -0500

    testing subdomain

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://formspree.io/katebalducci@gmail.com"
+                // url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
